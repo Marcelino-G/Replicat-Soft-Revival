9a764e75817ac15e929994313c4dc28e8e128f4a
Marcelino Gamino <marcelino_gamino@yahoo.com>
2022-10-02 18:33:57 -0700

    about us text content inserted and 'rain' animation implemented

--- src/routes/aboutus.js
+++ src/routes/aboutus.js
@@ -1,47 +1,60 @@
-// const handleOnClickMes = (e) => {
-//     let eldonMes = document.getElementById('eldonMessage')
-//     let mes = e.target.textContent
-//     let mesArray = Array.from(mes)
 
-//     for(let i=0; i<mesArray.length; i++){
-//       let letter = document.createElement('p')
-//       if (mesArray[i] === " "){
-//         letter.textContent = '\u00A0';
-//       } else {
-//         letter.textContent = mesArray[i]
-//       }
+const aboutUsMessage = "Over a decade ago, the incendent between man and machine left\
+ people all around the world questioning the advancements of technology.\
+ While the majority was regressing to a past time type of living, THE SIBER CORPORATION was\
+ working day and night to prove that this descent was not the answer. After countless trials and errors\
+ to accomplish the perfect machine, the first lineup of REPLICATS were made.\
+ This is a reintroduction to the machine that was previously poorly maintained by\
+ a company who lacked the integrity and knowledge to be developing and distributing\
+ such an instrument. This is only a sample of what's to come and is the beginning\
+ of the machine's soft revival. ";
 
-//       if (i % 6 === 0){
-//         letter.classList.add('fun')
-//       } else if (i % 5 === 0) {
-//         letter.classList.add('fun1')
-//       } else if (i % 4 === 0){
-//         letter.classList.add('fun2')
-//       } else if (i % 3 === 0){
-//         letter.classList.add('fun3')
-//       } else if (i % 2 === 0){
-//         letter.classList.add('fun4')
-//       } else if (i % 1 === 0){
-//         letter.classList.add('fun5')
-//       }
+let aboutUsWord = '';
+const messageArray = [];
 
-//       eldonMes.append(letter)
-//     }
-//   }
-
-
-
+for(let i = 0; i < aboutUsMessage.length; i++){
+    if(aboutUsMessage[i] === " "){
+        messageArray.push(aboutUsWord)
+        aboutUsWord = ''
+    }
+    aboutUsWord = `${aboutUsWord + aboutUsMessage[i]}`;
+}
 
 const AboutUs = () => {
-
     return (
         <section id='aboutUs' className="border col-8">
             <h2>
                 About Us
             </h2>
-            <p>
-                hello there
-            </p>
+            <div>
+                {
+                    messageArray.map((word, wordIndex) => {
+                        let wordArray = Array.from(word);
+                            return (
+                            <div className="d-inline-block"> 
+                                {wordArray.map((letter, letterIndex) => {
+                                    if (letterIndex % 6 === 0){
+                                        return (<span key={letterIndex + letter} className='rain' >{letter}</span>)
+                                    } else if (letterIndex % 5 === 0) {
+                                        return (<span key={letterIndex + letter} className='rain1'>{letter}</span>)
+                                    } else if (letterIndex % 4 === 0){
+                                        return (<span key={letterIndex + letter} className='rain2'>{letter}</span>)
+                                    } else if (letterIndex % 3 === 0){
+                                        return (<span key={letterIndex + letter} className='rain3'>{letter}</span>)
+                                    } else if (letterIndex % 2 === 0){
+                                        return (<span key={letterIndex + letter} className='rain4'>{letter}</span>)
+                                    } else if (letterIndex % 1 === 0){
+                                        return (<span key={letterIndex + letter} className='rain5'>{letter}</span>)
+                                    }
+                                })}
+                                &nbsp;
+                            </div>
+                        )
+                        
+
+                    })
+                }
+            </div>
         </section>
     )
 }
