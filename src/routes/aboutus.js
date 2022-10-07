1fea096ad8a95875a62e46c7b598bf6009e165ba
Marcelino Gamino <marcelino_gamino@yahoo.com>
2022-10-07 15:52:10 -0700

    attribute work

--- src/routes/aboutus.js
+++ src/routes/aboutus.js
@@ -54,8 +54,8 @@ const AboutUs = () => {
                     })
                 }
             </div>
-            <button id="upArrow" className="col-2">
-                <a href="#top">
+            <button aria-label="Up arrow" id="upArrow" className="col-2">
+                <a aria-label="To the top" href="#top">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-up-circle" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                     </svg>
